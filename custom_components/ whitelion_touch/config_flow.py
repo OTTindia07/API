ece446a144cc91bb0c1a8e4f824640b64373421b
OTTindia07 <meetzankhariyaking@gmail.com>
2024-11-18 06:16:34 -0800

    Update config_flow.py

--- custom_components/ whitelion_touch/config_flow.py
+++ custom_components/ whitelion_touch/config_flow.py
@@ -1,13 +1,23 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
+from .api import WhitelionTouchAPI
 
 class WhitelionTouchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input["device_id"], data=user_input)
+            # Validate the input by logging into the device
+            api = WhitelionTouchAPI(
+                user_input["host"], user_input["device_id"], user_input["username"], user_input["password"]
+            )
+            response = await self.hass.async_add_executor_job(api.login)
+            if response.get("error") == 0:
+                return self.async_create_entry(title=user_input["device_id"], data=user_input)
+            errors["base"] = "cannot_connect"
 
         return self.async_show_form(
             step_id="user",
@@ -16,5 +26,6 @@ class WhitelionTouchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required("device_id"): str,
                 vol.Optional("username", default="admin"): str,
                 vol.Optional("password", default="1234"): str,
-            })
+            }),
+            errors=errors
         )
