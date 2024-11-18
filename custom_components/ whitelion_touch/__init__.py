cb2612496bf6c2199fd9e0b30f84c1d81243a803
OTTindia07 <meetzankhariyaking@gmail.com>
2024-11-18 05:56:13 -0800

    Create __init__.py

---
+++ custom_components/ whitelion_touch/__init__.py
@@ -0,0 +1,20 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+from .api import WhitelionTouchAPI
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Whitelion Touch from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = WhitelionTouchAPI(
+        entry.data["host"], entry.data["device_id"]
+    )
+
+    hass.config_entries.async_setup_platforms(entry, ["switch", "sensor"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_unload_platforms(entry, ["switch", "sensor"])
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
