21b7b1d7e33ebdd3e7d24cef72ac6419e6e9250f
OTTindia07 <meetzankhariyaking@gmail.com>
2024-11-18 05:57:58 -0800

    Create sensor.py

---
+++ custom_components/ whitelion_touch/sensor.py
@@ -0,0 +1,24 @@
+from homeassistant.components.sensor import SensorEntity
+from .const import DOMAIN
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    api = hass.data[DOMAIN][config_entry.entry_id]
+    async_add_entities([TouchPanelStatusSensor(api)])
+
+class TouchPanelStatusSensor(SensorEntity):
+    def __init__(self, api):
+        self.api = api
+        self._status = None
+
+    @property
+    def name(self):
+        return "Touch Panel Status"
+
+    @property
+    def state(self):
+        return self._status
+
+    async def async_update(self):
+        response = self.api.get_status()
+        if response.get("error") == 0:
+            self._status = response.get("data")
