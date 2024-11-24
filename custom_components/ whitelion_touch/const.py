b6732adf056989e18ab71046f223672afb99ef2e
OTTindia07 <meetzankhariyaking@gmail.com>
2024-11-24 05:37:11 -0800

    Update const.py

--- custom_components/ whitelion_touch/const.py
+++ custom_components/ whitelion_touch/const.py
@@ -1,5 +1,11 @@
-"""Constants for the Whitelion Touch integration."""
 DOMAIN = "whitelion_touch"
 CONF_DEVICE_ID = "device_id"
 CONF_IP_ADDRESS = "ip_address"
 CONF_MODEL = "model"
+
+# API Commands
+CMD_DEVICE_INFO = "DL"
+CMD_SWITCH_CONTROL = "ST"
+CMD_STATUS = "SS"
+
+DEFAULT_SCAN_INTERVAL = 30
