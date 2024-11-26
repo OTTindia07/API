e8e5a70bdd0a23178413db6850341a74320cecc8
OTTindia07 <meetzankhariyaking@gmail.com>
2024-11-26 06:29:45 -0800

    Update __init__.py

--- custom_components/ whitelion_touch/__init__.py
+++ custom_components/ whitelion_touch/__init__.py
@@ -1,21 +1,24 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from .const import DOMAIN
 
-from .const import DOMAIN, PLATFORMS
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Whitelion Touch integration."""
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Whitelion Touch integration from YAML (not supported)."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Whitelion Touch from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    # Forward the config entry to the switch platform
+    hass.config_entries.async_setup_platforms(entry, ["switch"])
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["switch"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
