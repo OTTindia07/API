fbc11d2cd8d8fe9fdd66614f359d2907551e6b18
OTTindia07 <meetzankhariyaking@gmail.com>
2024-11-26 06:33:20 -0800

    Update __init__.py

--- custom_components/ whitelion_touch/__init__.py
+++ custom_components/ whitelion_touch/__init__.py
@@ -12,8 +12,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    # Forward the config entry to the switch platform
-    hass.config_entries.async_setup_platforms(entry, ["switch"])
+    # Forward the entry setup to the switch platform
+    await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
