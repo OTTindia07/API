3643e636d1c1fb6354147c5d1a279b935c3fc855
OTTindia07 <meetzankhariyaking@gmail.com>
2024-11-26 04:06:07 -0800

    Update __init__.py

--- custom_components/ whitelion_touch/__init__.py
+++ custom_components/ whitelion_touch/__init__.py
@@ -1,17 +1,21 @@
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN
+
+from .const import DOMAIN, PLATFORMS
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Whitelion Touch integration."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Whitelion Touch from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
